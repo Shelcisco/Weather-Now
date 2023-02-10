35c6b06291e08b2b46092990a933d05d013bdeb3
Shelcisco <119394650+Shelcisco@users.noreply.github.com>
2023-02-09 23:19:22 -0800

    Update HTML/JavaScript Saving txt to local Storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,14 @@
-var APIKey = "a21690291032747ea02cca028da03d72";
-var city;
+// var APIKey = "a21690291032747ea02cca028da03d72";
 
 
+const input= document.querySelector("input");
+h2 = document.querySelector("h2");
 
+h2.innerHTML = localStorage.getItem ("city")
+
+input.addEventListener("keyup", display);
+
+function display(){
+localStorage.setItem('city', input.value)
+h2.innerHTML = localStorage.getItem ("city")
+}
