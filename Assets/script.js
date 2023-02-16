8b3af85167e4354fa8b4d5a253fd78b1482e45a3
Shelcisco <119394650+Shelcisco@users.noreply.github.com>
2023-02-15 22:24:05 -0800

    Update- Local Storage save Date/Location as array

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,52 @@
-// var APIKey = "a21690291032747ea02cca028da03d72";
+//Saving Location and Date to local Storage
 
+const searchHistory = [];
 
-const input= document.querySelector("input");
-h2 = document.querySelector("h2");
+const srchBttn = document.querySelector("#search-button");
 
-h2.innerHTML = localStorage.getItem ("city")
+srchBttn.addEventListener("click", saveSearch);
 
-input.addEventListener("keyup", display);
-
-function display(){
-localStorage.setItem('city', input.value)
-h2.innerHTML = localStorage.getItem ("city")
+function saveSearch(){
+  const locationInput = document.querySelector("#location-search").value;
+  const dateInput = document.querySelector("#datepicker").value;
+  const timestamp = Date.now();
+  
+  const search = {
+    location: locationInput,
+    date: dateInput
+  };
+  
+  localStorage.setItem(`search${timestamp}`, JSON.stringify(search));
+  searchHistory.push(search);
 }
+  
+ // get locations/dates from local storage
+const locations = JSON.parse(localStorage.getItem('Locations')) || [];
+const dates = JSON.parse(localStorage.getItem('Date')) || [];
+
+// combine locations and dates 
+const combined = [];
+for (let i = 0; i < locations.length; i++) {
+  const location = locations[i];
+  const date = dates[i];
+  const combinedStr = `${location} ${date}`;
+  combined.push(combinedStr);
+ 
+}
+
+// create new h4 elements for each combined string
+const recentSearchesDiv = document.getElementById('recent-searches');
+recentSearchesDiv.innerHTML = ''; 
+for (let i = 0; i < combined.length; i++) {
+  const h4 = document.createElement('h4');
+  const text = document.createTextNode(combined[i]);
+  h4.appendChild(text);
+  h4.classList.add('recent-search');
+  h4.addEventListener('click', function() {
+    const [location, date] = combined[i].split(' ');
+  });
+  recentSearchesDiv.appendChild(h4);
+  
+}
+
+
