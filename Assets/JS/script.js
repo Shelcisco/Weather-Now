95e60f5a99ef2e576228395abca34bb243a602dd
Shelcisco <119394650+Shelcisco@users.noreply.github.com>
2023-05-28 10:32:43 -0700

    ADD Get API Function

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,52 +1,77 @@
-//Saving Location and Date to local Storage
+var APIkey = 'a21690291032747ea02cca028da03d72';
+var cityInput = document.getElementById('city-input');
+// var submitBtn = document.querySelector('.btn');
+var SearchedCities = [];
+// var previousCityEl = document.getElementById('history');
+var weatherEl = document.querySelector('#results');
+var fivedayEl = document.querySelector('.five-day');
+var clearSearch =  document.getElementById('clearBtn')
 
-const searchHistory = [];
 
-const srchBttn = document.querySelector("#search-button");
+// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + "&appid=" + APIKey;
+// var WeatherApiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + inputValue.value + APIkey;
 
-srchBttn.addEventListener("click", saveSearch);
-
-function saveSearch(){
-  const locationInput = document.querySelector("#location-search").value;
-  const dateInput = document.querySelector("#datepicker").value;
-  const timestamp = Date.now();
-  
-  const search = {
-    location: locationInput,
-    date: dateInput
-  };
-  
-  localStorage.setItem(`search${timestamp}`, JSON.stringify(search));
-  searchHistory.push(search);
-}
-  
- // get locations/dates from local storage
-const locations = JSON.parse(localStorage.getItem('Locations')) || [];
-const dates = JSON.parse(localStorage.getItem('Date')) || [];
-
-// combine locations and dates 
-const combined = [];
-for (let i = 0; i < locations.length; i++) {
-  const location = locations[i];
-  const date = dates[i];
-  const combinedStr = `${location} ${date}`;
-  combined.push(combinedStr);
+function getApi() {
+  // fetch request gets a list of all the repos for the node.js organization
  
-}
-
-// create new h4 elements for each combined string
-const recentSearchesDiv = document.getElementById('recent-searches');
-recentSearchesDiv.innerHTML = ''; 
-for (let i = 0; i < combined.length; i++) {
-  const h4 = document.createElement('h4');
-  const text = document.createTextNode(combined[i]);
-  h4.appendChild(text);
-  h4.classList.add('recent-search');
-  h4.addEventListener('click', function() {
-    const [location, date] = combined[i].split(' ');
-  });
-  recentSearchesDiv.appendChild(h4);
   
-}
+    var city = cityInput.value;
+    var currentDay = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=' + APIkey;
+    fetch(currentDay)
+      .then(function (response) {
+        if (response.ok) {
+          console.log(response.status);
+          return response.json();
+        }
+      })
+      .then(function (data) {
+        console.log(data);
+        displayResults(data);
+        fiveDayForecast(data);
+        displayCities();
+      }); 
+};
 
 
+
+
+
+
+//Saving Location to local Storage
+
+// const Location= [];
+// const input= document.querySelector("#location-search");
+// const srchBttn = document.querySelector("#search-button");
+// const recentsearches = document.querySelector(".recent-searches")
+
+// // h4 = document.querySelector("h4");
+
+// srchBttn.addEventListener("click", display);
+
+// function display(){
+//   Location.push(input.value)    
+//   localStorage.setItem('Locations', Location)
+//   const newh4= document.createElement("h4")
+//   // newh4.innerHTML = localStorage.getItem ("Locations")
+//   newh4.innerHTML = input.value
+// recentsearches.append(newh4)
+//   }
+
+
+//   //Saving Dates to local storage 
+  
+//   const Day= [];
+// const input2= document.querySelector("#datepicker");
+// const srchBttn2 = document.querySelector("#search-button");
+
+// // h5 = document.querySelector("h5");
+
+// srchBttn2.addEventListener("click", Days);
+
+// function Days(){
+//   Day.push(input2.value)    
+//   localStorage.setItem('Date', Day)
+//   const newh5= document.createElement("h5")
+//   newh5.innerHTML = input2.value
+//   recentsearches.append(newh5)
+//   }
